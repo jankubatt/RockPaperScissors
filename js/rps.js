4378388f2ccfefc735ce4039d03f968a33d367ea
jan kubát <jankubat.1@seznam.cz>
2020-09-04 12:51:16 +0200

    border

--- js/rps.js
+++ js/rps.js
@@ -29,72 +29,72 @@ $("#stone").click(function () {
   var oponent = Math.floor(Math.random() * 3);
   var user = 0;
 
-  $(".items").css("border", "2px solid black");
+  $(".items").css("border", "3px solid black");
 
   switch (oponent) {
     case 0:
       $("#user").text("Tie");
-      $("#stone").css("border", "2px solid red");
+      $("#stone").css("border", "3px solid red");
       break;
 
     case 1:
       $("#user").text("Lost");
-      $("#paper").css("border", "2px solid red");
+      $("#paper").css("border", "3px solid red");
       break;
 
     case 2:
       $("#user").text("Won");
-      $("#shears").css("border", "2px solid red");
+      $("#shears").css("border", "3px solid red");
       break;
   }
 });
 
 $("#paper").click(function () {
-  $(".items").css("borded", "2px solid black");
+  $(".items").css("borded", "3px solid black");
   var oponent = Math.floor(Math.random() * 3);
   var user = 1;
 
-  $(".items").css("border", "2px solid black");
+  $(".items").css("border", "3px solid black");
 
   switch (oponent) {
     case 0:
       $("#user").text("Won");
-      $("#stone").css("border", "2px solid red");
+      $("#stone").css("border", "3px solid red");
       break;
 
     case 1:
       $("#user").text("Tie");
-      $("#paper").css("border", "2px solid red");
+      $("#paper").css("border", "3px solid red");
       break;
 
     case 2:
       $("#user").text("Lost");
-      $("#shears").css("border", "2px solid red");
+      $("#shears").css("border", "3px solid red");
       break;
   }
 });
 
 $("#shears").click(function () {
-  $(".items").css("borded", "2px solid black");
+  $(".items").css("borded", "3px solid black");
   var oponent = Math.floor(Math.random() * 3);
   var user = 2;
 
-  $(".items").css("border", "2px solid black");
+  $(".items").css("border", "3px solid black");
 
   switch (oponent) {
     case 0:
       $("#user").text("Lost");
-      $("#stone").css("border", "2px solid red");
+      $("#stone").css("border", "3px solid red");
       break;
 
     case 1:
       $("#user").text("Won");
-      $("#paper").css("border", "2px solid red");
+      $("#paper").css("border", "3px solid red");
       break;
 
     case 2:
       $("#user").text("Tie");
-      $("#shears").css("border", "2px solid red");
+      $("#shears").css("border", "3px solid red");
       break;
   }
 });
